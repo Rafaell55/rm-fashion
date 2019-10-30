05d775c9ab1c6332d856c5e1671b421a63a2f14b
Rafaell55 <rafael-souza4@hotmail.com>
2019-10-30 11:49:11 -0300

    Iniciando o Projeto de Loja

---
+++ src/main/java/com/rmfashion/RmFashionApplication.java
@@ -0,0 +1,13 @@
+package com.rmfashion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RmFashionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RmFashionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rmfashion/RmFashionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rmfashion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RmFashionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
